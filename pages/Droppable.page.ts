578b63d378890d3eb049242863a96503c41e452d
anastshak <nastyashakura@gmail.com>
2026-04-08 16:18:20 +0300

    test: add test for Droppable - Accept Tab

--- pages/Droppable.page.ts
+++ pages/Droppable.page.ts
@@ -1,15 +1,33 @@
 import { Locator, Page } from "@playwright/test";
 
+export const DROP_COLORS = {
+  default: "rgb(255, 255, 255)",
+  dropped: "rgb(70, 130, 180)",
+  hoverAccept: "rgb(60, 179, 113)",
+};
+export const DROP_TEXTS = {
+  defaultSimple: "Drop Here",
+  defaultAccept: "Drop here",
+  success: "Dropped!",
+};
+
 export class DroppablePage {
   readonly page: Page;
 
   // tabs
   readonly simpleTab: Locator;
+  readonly acceptTab: Locator;
 
   // simple tab elements
   readonly simpleTabContainer: Locator;
   readonly simpleDragElement: Locator;
   readonly simpleDropZone: Locator;
+
+  // accept tab elements
+  readonly acceptTabContainer: Locator;
+  readonly acceptDragElement: Locator;
+  readonly notAcceptDragElement: Locator;
+  readonly acceptDropZone: Locator;
 
   constructor(page: Page) {
     this.page = page;
@@ -18,17 +36,50 @@ export class DroppablePage {
     this.simpleTabContainer = page.locator("#simpleDropContainer");
     this.simpleDragElement = this.simpleTabContainer.locator("#draggable");
     this.simpleDropZone = this.simpleTabContainer.locator("#droppable");
+
+    this.acceptTab = page.locator("#droppableExample-tab-accept");
+    this.acceptTabContainer = page.locator("#acceptDropContainer");
+    this.acceptDragElement = this.acceptTabContainer.locator("#acceptable");
+    this.notAcceptDragElement =
+      this.acceptTabContainer.getByText("Not Acceptable");
+    this.acceptDropZone = this.acceptTabContainer.locator(".drop-box");
   }
 
   async open() {
     await this.page.goto("/droppable");
   }
 
+  /* simple tab actions */
   async openSimpleTab() {
     await this.simpleTab.click();
   }
 
   async simpleDragAndDrop() {
-    await this.simpleDragElement.dragTo(this.simpleDropZone);
+    // works unstable (try to drop to the center manually)
+    // await this.simpleDragElement.dragTo(this.simpleDropZone);
+
+    const box = await this.simpleDropZone.boundingBox();
+    if (!box) throw new Error("Drop zone not found");
+
+    await this.simpleDragElement.dragTo(this.simpleDropZone, {
+      targetPosition: {
+        x: box.width / 2,
+        y: box.height / 2,
+      },
+      force: true,
+    });
+  }
+
+  /* accept tab actions */
+  async openAcceptTab() {
+    await this.acceptTab.click();
+  }
+
+  async acceptDragAndDrop() {
+    await this.acceptDragElement.dragTo(this.acceptDropZone);
+  }
+
+  async notAcceptDragAndDrop() {
+    await this.notAcceptDragElement.dragTo(this.acceptDropZone);
   }
 }
--- tests/droppable.spec.ts
+++ tests/droppable.spec.ts
@@ -1,5 +1,9 @@
 import { test, expect } from "@playwright/test";
-import { DroppablePage } from "../pages/Droppable.page";
+import {
+  DROP_COLORS,
+  DROP_TEXTS,
+  DroppablePage,
+} from "../pages/Droppable.page";
 
 test.describe("Droppable Functionality", () => {
   let droppablePage: DroppablePage;
@@ -10,19 +14,22 @@ test.describe("Droppable Functionality", () => {
   });
 
   test.describe("Simple Tab", () => {
-    test.beforeEach(async ({}) => {
+    test.beforeEach(async () => {
       await droppablePage.openSimpleTab();
     });
+
     test("should drag and drop element successfully", async () => {
-      await expect(droppablePage.simpleDropZone).toHaveText("Drop Here");
+      await expect(droppablePage.simpleDropZone).toHaveText(
+        DROP_TEXTS.defaultSimple,
+      );
 
       await droppablePage.simpleDragAndDrop();
 
-      await expect(droppablePage.simpleDropZone).toHaveText("Dropped!");
+      await expect(droppablePage.simpleDropZone).toHaveText(DROP_TEXTS.success);
 
       await expect(droppablePage.simpleDropZone).toHaveCSS(
         "background-color",
-        "rgb(70, 130, 180)",
+        DROP_COLORS.dropped,
       );
     });
 
@@ -32,7 +39,66 @@ test.describe("Droppable Functionality", () => {
       });
 
       await expect(droppablePage.simpleDragElement).toHaveText("Drag Me");
-      await expect(droppablePage.simpleDropZone).toHaveText("Drop Here");
+      await expect(droppablePage.simpleDropZone).toHaveText(
+        DROP_TEXTS.defaultSimple,
+      );
+    });
+  });
+
+  test.describe("Accept Tab", () => {
+    test.beforeEach(async () => {
+      await droppablePage.openAcceptTab();
+    });
+
+    test("should accept only valid draggable element", async () => {
+      await expect(droppablePage.acceptDropZone).toHaveText(
+        DROP_TEXTS.defaultAccept,
+      );
+
+      await droppablePage.acceptDragAndDrop();
+
+      await expect(droppablePage.acceptDropZone).toHaveText(DROP_TEXTS.success);
+
+      await expect(droppablePage.acceptDropZone).toHaveCSS(
+        "background-color",
+        DROP_COLORS.dropped,
+      );
+    });
+
+    test("should NOT accept invalid draggable element", async () => {
+      await droppablePage.notAcceptDragAndDrop();
+
+      await expect(droppablePage.acceptDropZone).toHaveText(
+        DROP_TEXTS.defaultAccept,
+      );
+    });
+
+    test("should accept valid element after invalid attempt", async () => {
+      await droppablePage.notAcceptDragAndDrop();
+      await expect(droppablePage.acceptDropZone).toHaveText(
+        DROP_TEXTS.defaultAccept,
+      );
+
+      await droppablePage.acceptDragAndDrop();
+      await expect(droppablePage.acceptDropZone).toHaveText(DROP_TEXTS.success);
+    });
+
+    test("should change color to green on hover and blue on drop", async ({
+      page,
+    }) => {
+      await droppablePage.acceptDragElement.hover();
+      await page.mouse.down();
+      await droppablePage.acceptDropZone.hover();
+      await expect(droppablePage.acceptDropZone).toHaveCSS(
+        "background-color",
+        DROP_COLORS.hoverAccept,
+      );
+      await page.mouse.up();
+      await expect(droppablePage.acceptDropZone).toHaveCSS(
+        "background-color",
+        DROP_COLORS.dropped,
+      );
+      await expect(droppablePage.acceptDropZone).toHaveText(DROP_TEXTS.success);
     });
   });
 });
